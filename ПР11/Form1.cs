5ba40560942d0c9ef85ee895133d8ec0986f2c13
ilyasor1337 <134691644+ilyasor1337@users.noreply.github.com>
2023-05-26 12:24:04 +0300

    Add files via upload

---
+++ ПР11/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ПР11
+{
+    public partial class Form1 : Form
+    {
+
+        private Bitmap[] Frames;
+        private int FrameNum = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Frames = new Bitmap[18];
+            for(int i = 0; i < 18; i++)
+            {
+                Frames[i] = new Bitmap(@"D:\207 Забелина Хохлов\РПМ ПР\ПР11\Frame" + i + ".png");
+
+            }
+            pictureBox1.Image = Frames[FrameNum];
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) 
+        {
+            FrameNum = ++FrameNum % Frames.Length;
+            pictureBox1.Image = Frames[FrameNum];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            if (timer1.Enabled) button1.Text = "stop";
+            else button1.Text = "start";
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timer1.Interval = 100 / trackBar1.Value;
+
+        }
+    }
+}
